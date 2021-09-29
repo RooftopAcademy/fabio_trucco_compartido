7b95b8851c83bceb9487ade49d1feb4a1e536784
Fabio Martín Trucco <85174494+FabioTrucco@users.noreply.github.com>
2021-09-29 15:25:23 -0300

    refactored the magical numbers on scrollDown func

--- src/helpers/scrollDown.ts
+++ src/helpers/scrollDown.ts
@@ -4,11 +4,14 @@ export default function scrollDown(): void {
   
     let y = window.scrollY;
     let x = window.innerWidth;
-  
-    if (y >= 80 && x >= 1280 && asideMenu) {
+
+    let downPixels: number = 80;
+    let widthPixels: number = 1280;
+    
+    if (y >= downPixels && x >= widthPixels) {
         asideMenu.className = "aside-menu show";
     } 
     else {
         asideMenu.className = "aside-menu";
     }
-  };
+}
