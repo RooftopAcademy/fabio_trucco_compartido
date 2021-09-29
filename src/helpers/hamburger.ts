3e1dda0fea5c88094349201855aa021ee14fda0e
Fabio Martín Trucco <85174494+FabioTrucco@users.noreply.github.com>
2021-09-29 14:49:21 -0300

    refactored hamburguer.ts

--- src/helpers/hamburger.ts
+++ src/helpers/hamburger.ts
@@ -1,19 +1,7 @@
+export default function displayHamburgerMenu(): void {
 
-export default function displayHamburgerMenu(): void{
-    let menuClick: number = 0;
-    document.getElementById("hamburger")
-      .addEventListener("click", function (e): void { 
-        e.preventDefault
-        const navMenu = document.getElementsByClassName("nav-menu")[0];
-        menuClick += 1;
-        if (menuClick == 1){
-        (navMenu as HTMLElement).className = "nav-menu active";
-        }
-        else{
-        (navMenu as HTMLElement).className = "nav-menu"
-        menuClick = 0;
-        }
-    })
+    const navMenu: HTMLElement = document.getElementsByClassName("nav-menu")[0] as HTMLElement;
+    
+    document.getElementById("hamburger")?.addEventListener("click", () => navMenu.classList.toggle("active"));
 }
-
-
+    
