c3c47d1c6137a273e2ab36ce3a0afcff4318f268
Fabio Martín Trucco <85174494+FabioTrucco@users.noreply.github.com>
2021-10-01 10:01:10 -0300

    fixed

--- src/helpers/multipleChecks.ts
+++ src/helpers/multipleChecks.ts
@@ -2,7 +2,7 @@ import isChecked from "./isChecked";
 
 export default function multipleChecks(ids: string[]) : string[] {
 
-    let answer: string[];
+    let answer: string[] = [];
     
     ids.forEach((id) => {
        if ( isChecked(id) ) { answer.push(id) } 
