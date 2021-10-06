5949e4b16cc7c72de08a105f1a2df1db9719ad79
Fabio Martín Trucco <85174494+FabioTrucco@users.noreply.github.com>
2021-10-06 12:07:13 -0300

    corrected some links

--- src/components/asideMenuComponent.ts
+++ src/components/asideMenuComponent.ts
@@ -3,9 +3,9 @@ export default function asideMenuComponent(): string {
   <div class="aside-menu" id="aside-menu">
       <nav class="main-header-links aside">
           <ul>
-              <li><a href="index.html">HOME</a></li>
-              <li><a href="store.html">STORE</a></li>
-              <li><a href="about.html">ABOUT</a></li>
+              <li><a href="#/">HOME</a></li>
+              <li><a href="#/store">STORE</a></li>
+              <li><a href="#/about">ABOUT</a></li>
               <li><a href="#">
                   <div class="cart-link">
                       <img class="shopping-cart"src="/images/shopping-cart.png" alt="cart"><span class="cart-number">0</span>
@@ -13,5 +13,6 @@ export default function asideMenuComponent(): string {
               </a></li>
           </ul>
       </nav>
-  </div>`
+  </div>
+  `;
 }
