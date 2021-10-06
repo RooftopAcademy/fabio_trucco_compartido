ae4a37e8e936857aeb69c687face69077a6d5118
Fabio Martín Trucco <85174494+FabioTrucco@users.noreply.github.com>
2021-10-06 08:58:56 -0300

    migrated the events to this page

--- src/pages/SignUpPage.ts
+++ src/pages/SignUpPage.ts
@@ -1,5 +1,15 @@
 import PageInterface from "../interfaces/PageInterface";
-import signUpComponent from '../components/signUpComponent'
+import signUpComponent from '../components/signUpComponent';
+import displayHamburgerMenu from '../helpers/hamburger';
+import backToTop from '../helpers/backToTop';
+import read from '../helpers/readRegisterFormInputs';
+import runSuccessStylingOnRegisterForm from '../helpers/runSuccessStylingOnRegisterForm';
+import revertSuccessStylingOnRegisterForm from '../helpers/revertSuccessStylingOnRegisterForm';
+import showMessageOnUserSubmit from '../helpers/showMessageOnUserSubmit';
+import RegisteredUserFactory from "../factories/RegisteredUserFactory";
+import UserInputError from "../entities/UserInputError";
+import showError from '../helpers/showError';
+import { store } from '../index';
 
 export default class SignUpPage implements PageInterface{
 
@@ -7,8 +17,48 @@ export default class SignUpPage implements PageInterface{
         c.innerHTML = signUpComponent();
     }
 
-    registerEvents(d : Document): void {
+    registerEvents(): void {
+
+        displayHamburgerMenu();
+        
+        backToTop();
+
+        submitUser();
 
     }
 
 }
+
+
+function submitUser(): void {
+
+    let submitButton: HTMLElement = document.getElementById("submit") as HTMLElement;
+  
+    submitButton?.addEventListener("click", function(e): void {
+      
+        e.preventDefault();
+
+        try{
+
+          let newRUser = RegisteredUserFactory.create(store, read())
+
+          store.fetchUser(newRUser);
+
+          runSuccessStylingOnRegisterForm();
+
+          showMessageOnUserSubmit('submit-click', 'Great! We have sent you an emil to confirm your account', 'success-message');
+
+          setTimeout( revertSuccessStylingOnRegisterForm, 3000 );
+
+        }
+        catch(err) {
+
+          if (err instanceof UserInputError) {
+
+            return showError(err.getInputId(), err.getContainertId(), err.getMessage())
+
+          }
+          alert(err.message)
+        }
+    })
+  }
