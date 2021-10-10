a0d596b658599c2e96ff715e221d7cac313c3f9c
Fabio Martín Trucco <85174494+FabioTrucco@users.noreply.github.com>
2021-10-10 12:02:42 -0300

    added count amount of products function to cart class

--- src/entities/Cart.ts
+++ src/entities/Cart.ts
@@ -4,29 +4,58 @@ export default class Cart {
 
     private _products: Map<Product, number>;  // It saves the product and its amount
 
-    constructor(){
+    constructor() {
+
         this._products = new Map;
+
     }
 
     public getProducts(): Map<Product, number> {
+
         return this._products;
+
     } 
 
-    public addProduct(p: Product, amount: number): void {
-        this._products.set(p, amount);
+    public addProduct(p: Product, amount: number = 1): void {
+
+        /**
+         * If product is not in the Map, then set it with amount = 1 by default.
+         * Else increment by 1 its value(amount).
+         */
+
+        if(!this._products.has(p)){
+            this._products.set(p, amount);
+        } else {
+            for (let [key, value] of Array.from(this._products)) {
+                if( key == p ){
+                    value++;
+                }
+            }
+        }
+        
     }
 
     public findProductById(id: number): Product {
+
         return Array.from(this._products.keys()).find((p) =>{ 
             return p.getId() == id;
         });
+
     }
 
-    public discartProduct(id: number): void {
+    public discardProduct(id: number): void {
 
         Array.from(this._products.keys()).filter((p) => {
             p.getId() != id; 
         })
+    }
+
+    public countProducts() : number {
+
+        return Array.from(this._products.values()).reduce((a, b) => {
+            return a+b;
+        })
+
     }
 
 }
