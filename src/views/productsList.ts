37eb7463085dd4ca71fd4c6d3c963c1caa87747e
Fabio Martín Trucco <85174494+FabioTrucco@users.noreply.github.com>
2021-10-10 18:31:37 -0300

    adapted the function to a Product[]

--- src/views/productsList.ts
+++ src/views/productsList.ts
@@ -1,7 +1,7 @@
 import productItem from "./productItem";
 import Product from "../entities/Product";
 
-export default function productsList(products: Product[]): string[]{
+export default function productsList(products: Product[]): string[] {
     return products.map((product) => productItem(product));
 }
 
