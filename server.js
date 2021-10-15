29ae23ad764deb804d5b6dcb64efae6abbeb29ea
Fabio Martín Trucco <85174494+FabioTrucco@users.noreply.github.com>
2021-10-15 09:32:35 -0300

    the server also works as an api, in requests you can visualize its functionalities

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const path = require('path');
+const data = require('./src/data/products.json')
 const express = require('express');
 const dotenv = require('dotenv')
 const app = express();
 
+
 dotenv.config()
 
+app.use(express.json());
+
+app.use(express.urlencoded());
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use("/dist", express.static(__dirname + "/dist"));
 
 
 app.get('/', (req, res) => {
@@ -16,15 +24,86 @@ app.get('/', (req, res) => {
 
 })
 
-app.use("/dist", express.static(__dirname + "/dist"));
+// API  
+
+app.get('/api/data', (req, res) => {
+
+  res.json(data)
+
+})
+
+app.get('/api/data/:id', (req, res) => {
+
+  const id = Number(req.params.id);
+
+  const product = data.find(product => product.id === id);
+
+  if (product) {
+
+      res.json(product);
+
+  } else {
+
+      res.status(404).end('Not found');
+
+  }
+
+})
+
+
+app.delete('/api/data/:id', (req, res) => {
+
+  const id = req.params.id;
+
+  data = data.filter(product => product.id != id);
+
+  res.status(204).end('No content')
+
+})
+
+app.post('/api/data', (req, res) => {
+
+  const product = req.body;
+
+  if(!product || !product.content) {
+      return res.status(400).json({
+          error: 'product.content is missing'
+      })
+  }
+
+  const ids = data.map(product => product.id)
+  const maxId = Math.max(...ids)
+
+  const newProduct = {
+
+      id : maxId++,
+      "name": product.name,
+      "maker": product.maker,
+      "material": product.material,
+      "theme": product.theme,
+      "price": product.price,
+      "stock": product.stock,
+      "stars": product.stars,
+      "description": product.description,
+      "image": product.image,
+      "buys": product.buys,
+      "guarantee": product.guarantee
+  }
+
+  data = [...data, newProduct]
+
+  res.json(newProduct);
+})
+
 
 const PORT = process.env.PORT;
 
 app.locals.baseuUrl = process.env.HOST + ':' + PORT;
 
-app.listen(3500, () => {
 
-  console.log('Server on port 3500')
+
+app.listen(PORT, () => {
+
+  console.log(`Server on port ${PORT}`)
 
 })
-
